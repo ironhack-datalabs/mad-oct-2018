a833b66f9fb610de9188ff843b221c5086674958
ijcernicharo <ivan.cernicharo@gmail.com>
2018-10-26 00:42:12 +0200

    lab-errhand_listcomp MOD #7

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -14,38 +14,44 @@ import os,random,sys
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-square_num=[e**2 for e in range(20)]
+square=[e**2 for e in range(20)]
+print(square)
 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-pow_2=[2**e for e in range(50)]
+power_of_two=[2**e for e in range(50)]
+print(power_of_two)
 
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
-square_root=[e**0.5 for e in range(100)]
+sqrt=[e**0.5 for e in range(100)]
+print(ssqrt)
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-list_4=[e-10 for e in range(11)]
+my_list=[e-10 for e in range(11)]
+print(my_list)
 
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-odd_num=[e for e in range(1,101) if e%2!=0]
+odds=[e for e in range(1,101) if e%2!=0]
+print(odds)
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-multiples_7=[e for e in range(1,1001) if e%7==0]
+divisible_by_7=[e for e in range(1,1001) if e%7==0]
+print(divisible_by_7)
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
@@ -53,8 +59,8 @@ multiples_7=[e for e in range(1,1001) if e%7==0]
 # You can use the following test string but feel free to modify at your convenience
 
 teststring = 'Find all of the words in a string that are monosyllabic'
-list_voewls=''.join([e for e in teststring if e not in 'aeiouAEIOU'])
-print(list_voewls)
+non_voewls=''.join([e for e in teststring if e not in 'aeiouAEIOU'])
+print(non_voewls)
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
@@ -79,7 +85,8 @@ print(consonants)
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
-madrid_oct_2018_files=[e for e in os.listdir('../../../') if os.path.isdir(os.path.join('../../../',e))]
+files=[e for e in os.listdir('../../../') if os.path.isdir(os.path.join('../../../',e))]
+print(files)
 
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
@@ -87,14 +94,15 @@ madrid_oct_2018_files=[e for e in os.listdir('../../../') if os.path.isdir(os.pa
 # Remember to use list comprehensions and to print your results
 
 random_lists=[random.sample(range(1, 100), 10) for f in range(4)]
+print(random_lists)
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
-
 flatten_list=[y for x in list_of_lists for y in x]
+print(flatten_list)
 
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
@@ -202,20 +210,61 @@ def linux_interaction():
 # Hint: we need to continually keep checking until we get an integer.
 # Use a while loop with a try,except, else block to account for incorrect inputs.
 
-
+def function_square_integer(counter=0):
+    while 1:
+        print("--------------------------------------------------")
+        counter+=1
+        integer=input('Iteration %s | Define the integer you want to obtain its square: ' %counter)
+        try:
+            int(float(integer))
+        except:
+            print("")
+            print("FUNCTION DOES NOT ACCEPT STR FORMAT!")
+            print("")
+        else:
+            try:
+                if int(float(integer))!=float(integer):
+                    raise TypeError
+            except TypeError:
+                print("")
+                print("You must define the input as INT type")
+                print()
+                print("Your actual data type is: ",type(float(integer)))
+                print("")
+            else:
+                return int(integer)**2
+                break
 
 
 # 22. Find all of the numbers from 1-1000 that are divisible by any single digit besides 1 (2-9). 
 # Use results as the name of the list 
 
-
+results=[]
+for f in range(1,1001):
+    # we make a list of the digits that the number f is divisible for
+    g=['' if f%e!=0 else e for e in range(2,10)]
+    # Define a counter for digits different to ''
+    count=0
+    for item in g:
+        if item!='':
+            count+=1
+    if count!=0:
+        results.append(f)
 
 
 # 23. Define a customised exception to handle not accepted values. 
 # You have the following user inputs and the Num_of_sections can not be less than 2.
 # Hint: Create a class derived from the pre-defined Exception class in Python
 
-Total_Marks = int(input("Enter Total Marks Scored: ")) 
-Num_of_Sections = int(input("Enter Num of Sections: "))
+class HowManyTimesHaveToExplainThatINTisMandatory(Exception):
+   """Why the hell you keep trying inputing values types different from INT?"""
+   pass
 
+###########################################################
 
+try:
+    Total_Marks = int(input("Enter Total Marks Scored: ")) 
+    Num_of_Sections = int(input("Enter Num of Sections: "))
+except:
+    #print(HowManyTimesHaveToExplainThatINTisMandatory(Exception))
+    print("Why the hell you keep trying inputing values types different from INT?")
