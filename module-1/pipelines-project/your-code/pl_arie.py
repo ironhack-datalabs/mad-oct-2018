4454d278b4ee1891bede69b4d9ea58af1e030fe3
Arie Hassan <ariehassana@gmail.com>
2018-10-29 20:19:43 +0100

    terminado

--- module-1/pipelines-project/your-code/pl_arie.py
+++ module-1/pipelines-project/your-code/pl_arie.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
-year = int(input('Enter the year: '))
+year = int(input('Enter the year (2016 or 2017): '))
 month = int(input('Enter the month from january till september (1-11) if you dont want to filter by month insert, 0: ')) 
 title = "Winned matches in the {}th month of the year number {}".format(month,year)
 
@@ -17,42 +17,53 @@ data = acquire()
 
 ### Wrangling
 def wrangle(data): 
-    data["tourney_date"] = pd.to_datetime(data.tourney_date.astype("str")) # le doy formato de fecha a tourney date
-    data["tourney_month"] = data["tourney_date"].dt.month # creo una columna con el mes que figura en tourney date
-    filtered = data[data["tourney_month"] == month] # Filtro el dataframe por el mes que me interesa
+    data = data.dropna(thresh=10)
+    #data["tourney_date"] = pd.to_datetime(data.tourney_date.astype("str")) # le doy formato de fecha a tourney date
+    #data["tourney_month"] = data["tourney_date"].dt.month # creo una columna con el mes que figura en tourney date
+    data["tourney_date"] = data.tourney_date.astype("str") # cambio
+    data["tourney_date"] = data["tourney_date"].str.split(".",expand=True) # cambio
+    data["tourney_month"] = pd.to_datetime(data["tourney_date"]).dt.month
+    if month == 0:
+        filtered = data
+    else: 
+        filtered = data[data["tourney_month"] == month] # Filtro el dataframe por el mes que me interesa
     return filtered
-
+    
 filtered = wrangle(data)
 
 ### Analyzing
-def analyze(filtered):
-    grouped = filtered["winner_name"].value_counts().head(10) # cantidad de partidos ganados en el mes 
+def analize(filtered):
+    grouped = filtered["winner_name"].value_counts().head(5) # cantidad de partidos ganados en el mes 
 
     """ media de partidos ganados al mes por jugador"""
-    monthly_mean = round(data.groupby("winner_name")["winner_name"].count()/(len(set(data['tourney_month']))),1) 
+    monthly_mean = round(data.groupby("winner_name")["winner_name"].count()/(len(set(data['tourney_date']))),1) 
     pd_grouped = (pd.DataFrame(grouped)).reset_index(level=0)
-    pd_grouped = pd_grouped.rename(columns={"winner_name":"winned matches","index":"winner_name"})
+    pd_grouped = pd_grouped.rename(columns={"winner_name":"winned_matches","index":"winner_name"})
     df_mm = pd.DataFrame(monthly_mean)
     df_mm.index.names = ["winner"]
     df_mm = (df_mm.reset_index(level=0))
-    df_mm = df_mm.rename(columns={"winner_name":"avg winned matches per month","winner":"winner_name"})
+    df_mm = df_mm.rename(columns={"winner_name":"avg_winned_matches_per_month","winner":"winner_name"})
     df_merge = pd.merge(pd_grouped,df_mm,on="winner_name")
     return df_merge
 
-df_merge = analyze(filtered)
+df_merge = analize(filtered)
 
 ### Reporting
 def visualize(df_merge):  
-    player = df_merge["winner_name"].head(10)
-    winned_matches = df_merge["winned_matches"].head(10)
+    player = df_merge["winner_name"].head(5)
+    winned_matches = df_merge["winned_matches"].head(5)
     avg_winned_matches = df_merge["avg_winned_matches_per_month"].head(10)
     sns.set()
+    
     _= fig, ax = plt.subplots(figsize=(10,5))
     _ = plt.bar(player,winned_matches)
     _ = plt.bar(player,avg_winned_matches)
     _ = plt.xlabel("name of the player",fontsize=15)
     _= plt.ylabel("number of games",fontsize=15)
     _= plt.legend(labels=["Winned matches","Avg winned matches per month"])
-    _= plt.title("Winned matches in the month {}".format(month) + "\n", fontsize=16)
+    _= plt.title(title.format(month) + "\n", fontsize=16)
     plt.xticks(fontsize=10)
     plt.savefig(title + '.png')
+    
+visualize(df_merge)
+    
