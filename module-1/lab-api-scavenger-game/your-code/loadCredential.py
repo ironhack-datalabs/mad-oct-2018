a2086d0bbcf1813971630607db319785bd06e59a
AlbertoCastellanos <alberto.castellanos.naya@gmail.com>
2018-10-30 21:04:20 +0100

    lab api completed

---
+++ module-1/lab-api-scavenger-game/your-code/loadCredential.py
@@ -0,0 +1,17 @@
+import os
+
+#https://github.com/theskumar/python-dotenv
+from dotenv import load_dotenv
+
+def loadCredentials(loadVars):
+    load_dotenv()
+    obj = dict()
+    for v in loadVars:
+        obj[v] = os.getenv(v)
+        if not obj[v]:
+            raise ValueError("env var '%s' does not exist. Please create a .env file containing it" % (v))
+    return obj
+
+if __name__ == "__main__":
+    requestKeys = ["GITHUB_TOKEN"]
+    d = loadCredentials(requestKeys)
